f0719c715210aa6722517132f7fdecb83c616d4d
Nabeel Aziz <nabeelabhan@gmail.com>
2024-01-04 23:01:21 +0400

    Implemented a simple state machine for the player character; Added a move state and player character walking sprite.

--- Player-Character/character.gd
+++ Player-Character/character.gd
@@ -1,6 +1,23 @@
-extends Node2D
+extends CharacterBody2D
 
-#region player
+enum States {
+	MOVE,
+	ATTACK,
+	HURT
+}
+
+# Onready variables
+@onready var sprite = $AnimatedSprite2D
+
+# Variable
+var state: States = States.MOVE
+
+# Movement constants
+const MAX_SPEED = 50
+const ACCELERATION = 400
+const FRICTION = 500
+
+#region Player
 var player = null:
 	set(value):
 		player = value
@@ -37,3 +54,37 @@ func input_get_input_released(action) -> bool:
 		return player.input_get_input_released(action)
 	return false
 #endregion
+
+#region States
+func move_state(delta) -> void:
+	var input_vector = Vector2.ZERO
+	input_vector.x = input_get_action_strength("Right") - input_get_action_strength("Left")
+	input_vector.y = input_get_action_strength("Down") - input_get_action_strength("Up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		move_and_slide()
+		sprite.play("default")
+		if input_vector.x == -1 or input_vector.x == 1:
+			sprite.scale = Vector2(input_vector.x, 1)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		sprite.stop()
+		sprite.frame = 0
+
+func attack_state() -> void:
+	pass
+
+func hurt_state() -> void:
+	pass
+#endregion
+
+func _physics_process(delta: float) -> void:
+	match (state):
+		States.MOVE:
+			move_state(delta)
+		States.ATTACK:
+			attack_state()
+		States.HURT:
+			hurt_state()
